30afd585f980fdc9e24540bc8395d88074c944ed
FelixEdenborgh <Felix@edenborgh.se>
2024-03-19 19:51:31 +0100

    Håller på med Garage och försöker förstå mig på IEnumerable

--- Garage_Ovn5/Garage.cs
+++ Garage_Ovn5/Garage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace Garage_Ovn5
 {
-    internal class Garage
+    public class Garage<T> : IEnumerable<T> where T : Vehicle
     {
+        private T[] vehicles;
+        private int capacity;
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+
+        // Generisk version av GetEnumerator som används för IEnumerable<T>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return vehicles[i];
+            }
+        }
+
+        // Icke-generisk version av GetEnumerator som används för IEnumerable
+        // Notera att detta är rätt sätt att anropa den generiska versionen för icke-generiska IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator(); // Ingen yield return här. Direkt anrop till den generiska GetEnumerator()
+        }
     }
 }
